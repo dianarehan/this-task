f091eb1ccf14387f444a89fb54a4ae15594d1924
diana <daiana.rehan.dr@gmail.com>
2024-07-03 12:38:49 +0300

    toggle between the supervisor and nomral

--- this-task/Assets/Scripts/DragObject.cs
+++ this-task/Assets/Scripts/DragObject.cs
@@ -8,6 +8,25 @@ public class DragObject : MonoBehaviour
 
     private Vector3 screenPoint;
     private Vector3 offset;
+    private bool isSupervisor;
+    private bool isNormal;
+
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            setNormal();
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            setSupervisor();
+        }
+    }
+
 
     void OnMouseDown()
     {
@@ -18,12 +37,34 @@ public class DragObject : MonoBehaviour
     }
 
     void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+    {   
 
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+        if((isNormal&& gameObject.CompareTag("blueObject")) || isSupervisor)
+        {
+            
+                Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+                Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+                transform.position = curPosition;
+
+            
+        }
+        
+        
 
     }
 
+
+    private void setSupervisor()
+    {
+        
+        isSupervisor = !isSupervisor;
+        isNormal = !isSupervisor;
+    }
+
+
+    private void setNormal()
+    {
+        isNormal = !isNormal;
+        isSupervisor = !isNormal;
+    }
 }
